2df8515289acd491ab2eac4ed76013a98750b7e1
upitroma <55750919+upitroma@users.noreply.github.com>
2023-10-29 23:17:02 -0400

    better movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,22 @@
 extends Node3D
 
 @export var maxSpeed = 50
-@export var pitchSpeed = 3
-@export var yawSpeed = 3
-@export var rollSpeed = 3
+@export var maxPitchSpeed = 3
+@export var pitchAcceleration = 3
+@export var maxYawSpeed = 3
+@export var yawAcceleration = 3
+@export var maxRollSpeed = 3
+@export var rollAcceleration = 3
 
 
+
+# velocity 
 var speed=0
+var pitchV=0
+var yawV=0
+var rollV=0
+
+# input tracking
 var pitch_up=0
 var pitch_down=0
 var yaw_left=0
@@ -19,6 +29,21 @@ var roll_right=0
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	pitchV+=pitchAcceleration*delta * (pitch_up-pitch_down)
+	pitchV=clamp(pitchV,-maxPitchSpeed,maxPitchSpeed)
+	
+	yawV+=yawAcceleration*delta * (yaw_left-yaw_right)
+	yawV=clamp(yawV,-maxYawSpeed,maxYawSpeed)
+	
+	rollV+=rollAcceleration*delta * (roll_right-roll_left)
+	rollV=clamp(rollV,-maxRollSpeed,maxRollSpeed)
+	
+	# drag
+	pitchV*=.99-delta
+	yawV*=.99-delta
+	rollV*=.99-delta
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -26,13 +51,13 @@ func _process(delta):
 	position+=global_transform.basis.z * speed*delta
 	
 	# pitch
-	rotate_object_local(Vector3(1, 0, 0), pitchSpeed*delta * (pitch_up-pitch_down))
+	rotate_object_local(Vector3(1, 0, 0), pitchV*delta)
 
 	# yaw
-	rotate_object_local(Vector3(0, 1, 0),yawSpeed*delta * (yaw_left-yaw_right))
+	rotate_object_local(Vector3(0, 1, 0),yawV*delta)
 	
 	# roll
-	rotate_object_local(Vector3(0, 0, 1),rollSpeed*delta * (roll_right-roll_left))
+	rotate_object_local(Vector3(0, 0, 1),rollV*delta)
 
 
 func _input(event):
